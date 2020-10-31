56b6f9582bec3b2bc2f4dbfb4a0f9489cd89ad34
crazypeace <665889+crazypeace@users.noreply.github.com>
2020-10-31 16:19:31 +0800

    Update 233v334.sh

--- 233v334.sh
+++ 233v334.sh
@@ -1,5 +1,6 @@
 apt-get update -y
 apt-get install -y lrzsz git zip unzip curl wget qrencode libcap2-bin dbus
+rm -rf /tmp/233boy/v2ray
 git clone https://github.com/crazypeace/v2ray -b "master" /tmp/233boy/v2ray --depth=1
 cd /tmp/233boy/v2ray
 chmod +x install.sh
