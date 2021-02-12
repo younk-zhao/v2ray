2f3090e2c35c7966d9ad17b78159e0f6c0bb8824
crazypeace <665889+crazypeace@users.noreply.github.com>
2021-02-12 12:14:23 +0800

    Update 233v334.sh

--- 233v334.sh
+++ 233v334.sh
@@ -1,5 +1,12 @@
-apt-get update -y
-apt-get install -y lrzsz git zip unzip curl wget qrencode libcap2-bin dbus
+if [[ -e "/usr/bin/yum" ]] 
+then
+  yum update -y
+  yum install -y lrzsz git zip unzip curl wget qrencode libcap
+else
+  apt-get update -y
+  apt-get install -y lrzsz git zip unzip curl wget qrencode libcap2-bin dbus
+fi
+
 rm -rf /tmp/233boy/v2ray
 git clone https://github.com/crazypeace/v2ray -b "master" /tmp/233boy/v2ray --depth=1
 cd /tmp/233boy/v2ray
